5a27ef23507800885412afdc3bc83255a48d5f0e
Halzyn <djgalax@gmail.com>
2018-01-31 12:28:50 +0100

    test 7 billion

--- main.js
+++ main.js
@@ -138,7 +138,7 @@ function accessCallback(response){
 				for (j in songies){
 					var id = songies[j]["track"]["id"]
 					var furl = 'https://api.spotify.com/v1/audio-features/06AKEBrKUckW0KREUWRnvT';
-					bsync_request.push(
+					async_request.push(
 						$.ajax({
 							url: furl,
 							headers: {
