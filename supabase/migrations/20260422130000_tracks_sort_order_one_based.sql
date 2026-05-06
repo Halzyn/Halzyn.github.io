43b81a4b2975d03b476a9600947f99e655702cb4
Halzyn <djgalax@gmail.com>
2026-05-07 00:58:15 +0200

    Delete old migrations

--- supabase/migrations/20260422130000_tracks_sort_order_one_based.sql
+++
@@ -1,8 +0,0 @@
-update public.tracks t
-set sort_order = t.sort_order + 1
-from (
-  select distinct contest_id
-  from public.tracks
-  where sort_order = 0
-) z
-where t.contest_id = z.contest_id;
